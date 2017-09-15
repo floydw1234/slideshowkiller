6f34229748f91d078474f3f07bb093d4f247af8f
floydw1234 <wfloyd@uci.edu>
2017-09-15 15:55:52 -0700

    recursion almost works. Nothing shows up though

--- content.js
+++ content.js
@@ -1,14 +1,39 @@
-var links = document.getElementsByTagName('a');
 
+var globalSite = document.URL.split(".com/")[0];
+var article = document.URL.split(".com/")[1].split("/")[0];
 //main();
+recursiveFill(document);
 
-function recursiveFill(link, CurrentTitle, NextTitle){
-    return;
+function recursiveFill(doc){
+    alert(doc.URL)
+    if(doc.URL.split(".com/")[1].split("/")[0] == article){
+        loop(doc,function(element){
+            request(element.href,function(xmlDoc){
+                //alert(xmlDoc.getElementsByTagName('a').length);
+                foreignElement = xmlDoc.body;
+                //alert(xmlDoc.body);
+                //alert("next thing Url:" + xmlDoc.URL);
+                header = foreignElement.getElementsByTagName('header')[0];
+                header.parentNode.removeChild(header);
+                images = foreignElement.getElementsByTagName('img');
+                for(i = 0; i < images.length ; i++){
+                    images[i].setAttribute('src', images[i].getAttribute('data-original'));
+                    images[i].removeAttribute('data-original');
+                }
+                temp = doc.createElement('div');
+                temp.innerHTML = foreignElement.innerHTML;
+                element.parentNode.replaceChild(temp, element);
+                return recursiveFill(xmlDoc);
+            });
+        });
+    }else{
+        return;
+    }
 }
 
 
-function loop(callBack){
-    var links = document.getElementsByTagName('a');
+function loop(doc, callBack){
+    var links = doc.getElementsByTagName('a');
     for (var i = 0, l = links.length; i < l; i++) {
         if(links[i].href.indexOf("lifebuzz.com") !== -1 && links[i].innerHTML.indexOf("Next") !== -1){
             callBack(links[i]);
@@ -17,8 +42,19 @@ function loop(callBack){
 }
 
 
+function request(url, callback){
+    //alert("hi");
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        callback(this.responseXML);
+    }
+    xhr.open("GET", url);
+    xhr.responseType = "document";
+    xhr.send();
+}
+/*
 
-loop(function(element){
+loop(function(element, doc){
     request(element.href,function(xmlDoc){
         foreignElement = xmlDoc.body;
         header = foreignElement.getElementsByTagName('header')[0];
@@ -32,19 +68,6 @@ loop(function(element){
         //alert(xmlDoc.title);
     });
 });
-
-
-function request(url,callback){
-    //alert("hi");
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        callback(this.responseXML);
-    }
-    xhr.open("GET", url);
-    xhr.responseType = "document";
-    xhr.send();
-}
-/*
 
 for (var i = 0, l = links.length; i < l; i++) {
     if(links[i].href.indexOf("lifebuzz.com") !== -1 && links[i].innerHTML.indexOf("Next") !== -1){
